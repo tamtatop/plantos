93905ff27176de7bd59cae688f827db301fe6452
tamtatop <ttopu18@freeuni.edu.ge>
2021-02-13 00:13:15 +0400

    essage=added top part of the plants page

--- plant.js
+++ plant.js
@@ -1,15 +1,55 @@
 export function render(jsonToRender){
  fetch(`/json/${jsonToRender}.json`).then(response => response.json()).then(data => {
     fetch('plant.html').then(resp=>resp.text()).then(html=>{
-      document.querySelector('.content').innerHTML=html
+      document.querySelector('.content').innerHTML=html;
       setText(data);
+      document.querySelector('.plCards').innerHTML=cardsAll(data.features);
+       document.querySelector('.plDesc').innerHTML=plantDescription(data.description);
+       document.querySelector('.plGallery').innerHTML=plantGallery(data.photos);
     });
   })
 }
 
 function setText(data){
   document.querySelector('.plTitle').innerHTML=data.name;
+  document.querySelector('.plSubtitle').innerHTML=data.otherNames;
 }
+
+function plantDescription(text) {
+  return text.split('\n').map(paragraph => 
+      `
+      <p>${paragraph}</p>
+      `
+    ).join('');
+}
+
+function plantGallery(images){
+  return images.map(img => 
+      `
+      <img src=${img}>
+      `
+    ).join('');
+}
+
+function plantCardComp(data){
+  return `
+    <div class="plTile">
+      <div class="plEmoji">
+        <img src="emojis/${data.icon}">
+      </div>
+      <div class="plCardInfo">
+        <div class="plCardTitle">${data.name}</div>
+        <div class="plCardText">${data.text}</div>
+      </div>
+    </div>
+  `
+}
+
+function cardsAll(data){
+  return data.map(subData => plantCardComp(subData)).join('');
+}
+
+
 
 
 // var slideIndex = 1;
